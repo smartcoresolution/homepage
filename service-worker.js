6f4a30d1c48e61988fdf89adb5d4ffc478c1a50a
Chang Kwan Jong <29265106+smartcoresolution@users.noreply.github.com>
2025-08-09 23:29:42 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 const CACHE_NAME = 'scs-cache-v1-2025-08-09T11:58:11.104482';
+const CACHE_NAME = 'scs-cache-v2-2025-08-09T11:58:11.104482';
 const ASSETS = ['./index.html','./styles.css'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
